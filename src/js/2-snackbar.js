d2e0120bec44f670278f531c1f0d24f3b11c4d3a
Zakhar-T <zakhar.null@gmail.com>
2025-03-11 10:46:23 +0200

    edit task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,29 +3,40 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const formEl = document.querySelector(".form");
 let delay = "";
-let state = "";
 
-formEl.addEventListener("input", getData);
+formEl.addEventListener("input", getDelay);
 formEl.addEventListener("click", createNotification);
 
-function getData(event) {
-    delay = Number(event.currentTarget.delay.value);
-    state = event.currentTarget.state.value;
+function getDelay(event) {
+    delay = Number(event.currentTarget.elements.delay.value);
 }
 
 function createNotification(event) {
-    event.preventDefault();
-    new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (state === "fulfilled") {
-                resolve(iziToast.success({
-                    message: `✅ Fulfilled promise in ${delay}ms`,
-                }));
-            } else if (state === "rejected") {
-                reject(iziToast.error({
-                    message: `❌ Rejected promise in ${delay}ms`,
-                }));
-            }
-        }, delay);
-    });
+    if (event.target.type === "submit") {
+        event.preventDefault();
+        const state = event.currentTarget.elements.state.value;
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (state === "fulfilled") {
+                    resolve(`✅ Fulfilled promise in ${delay}ms`);
+                } else {
+                    reject(`❌ Rejected promise in ${delay}ms`);
+                }
+            }, delay);
+        });
+        promise.then(value => {
+            iziToast.success({
+                message: value,
+                position: "topRight",
+                closeOnClick: true,
+            });
+        });
+        promise.catch(error => {
+            iziToast.error({
+                message: error,
+                position: "topRight",
+                closeOnClick: true,
+            });
+        });
+    };
 }
