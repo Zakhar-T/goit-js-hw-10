4664e7a168a6771d2fb71cf418a38bd8c9d1a298
Zakhar-T <zakhar.null@gmail.com>
2025-03-12 22:59:04 +0200

    2nd edit task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,32 +6,31 @@ const formEl = document.querySelector(".form");
 formEl.addEventListener("submit", createNotification);
 
 function createNotification(event) {
-    if (event.target.type === "submit") {
-        event.preventDefault();
-        const delay = Number(event.currentTarget.elements.delay.value);
-        const state = event.currentTarget.elements.state.value;
-        const promise = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (state === "fulfilled") {
-                    resolve(delay);
-                } else {
-                    reject(delay);
-                }
-            }, delay);
+    event.preventDefault();
+    const delay = Number(event.currentTarget.elements.delay.value);
+    const state = event.currentTarget.elements.state.value;
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+    promise.then(value => {
+        iziToast.success({
+            message: `✅ Fulfilled promise in ${value}ms`,
+            position: "topRight",
+            closeOnClick: true,
         });
-        promise.then(value => {
-            iziToast.success({
-                message: `✅ Fulfilled promise in ${value}ms`,
+    })
+        .catch(error => {
+            iziToast.error({
+                message: `❌ Rejected promise in ${error}ms`,
                 position: "topRight",
                 closeOnClick: true,
             });
-        })
-            .catch(error => {
-                iziToast.error({
-                    message: `❌ Rejected promise in ${error}ms`,
-                    position: "topRight",
-                    closeOnClick: true,
-                });
-            });
-    };
-}
+        });
+    formEl.reset();
+};
