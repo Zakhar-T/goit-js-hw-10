84380bd2c2d0ad1aa56a3b745215ecac927f1d8c
Zakhar-T <zakhar.null@gmail.com>
2025-03-12 09:52:14 +0200

    task 2 completed

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,41 +2,36 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 const formEl = document.querySelector(".form");
-let delay = "";
 
-formEl.addEventListener("input", getDelay);
 formEl.addEventListener("click", createNotification);
-
-function getDelay(event) {
-    delay = Number(event.currentTarget.elements.delay.value);
-}
 
 function createNotification(event) {
     if (event.target.type === "submit") {
         event.preventDefault();
+        const delay = Number(event.currentTarget.elements.delay.value);
         const state = event.currentTarget.elements.state.value;
         const promise = new Promise((resolve, reject) => {
             setTimeout(() => {
                 if (state === "fulfilled") {
-                    resolve(`✅ Fulfilled promise in ${delay}ms`);
+                    resolve(delay);
                 } else {
-                    reject(`❌ Rejected promise in ${delay}ms`);
+                    reject(delay);
                 }
             }, delay);
         });
         promise.then(value => {
             iziToast.success({
-                message: value,
+                message: `✅ Fulfilled promise in ${value}ms`,
                 position: "topRight",
                 closeOnClick: true,
             });
-        });
-        promise.catch(error => {
-            iziToast.error({
-                message: error,
-                position: "topRight",
-                closeOnClick: true,
+        })
+            .catch(error => {
+                iziToast.error({
+                    message: `❌ Rejected promise in ${error}ms`,
+                    position: "topRight",
+                    closeOnClick: true,
+                });
             });
-        });
     };
 }
